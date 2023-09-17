fc86bb4995f4b0a08f83e104aca136bf51c82773
Asami20 <asmaamouhcine123@gmail.com>
2023-09-17 15:16:57 -0700

    my first commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,15 @@
 int main(void)
 {
 int num1, num2;
-
 for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-putchar(num1 / 10 + '0'); // Print the tens digit of num1
-putchar(num1 % 10 + '0'); // Print the ones digit of num1
+putchar(num1 / 10 + '0');
+putchar(num1 % 10 + '0');
 putchar(' ');
-putchar(num2 / 10 + '0'); // Print the tens digit of num2
-putchar(num2 % 10 + '0'); // Print the ones digit of num2
-
+putchar(num2 / 10 + '0');
+putchar(num2 % 10 + '0');
 if (num1 != 98 || num2 != 99)
 {
 putchar(',');
@@ -29,9 +27,7 @@ putchar(' ');
 }
 }
 }
-
 putchar('\n');
 
 return (0);
 }
-
