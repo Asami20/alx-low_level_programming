58e5fda54bad9dc4a3352eef93a8f01ffbc65662
Asami20 <asmaamouhcine123@gmail.com>
2023-09-18 06:44:27 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+write(1, str, 1);
+str++;
+}
+write(1, "\n", 1);
+}
+
